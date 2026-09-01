c828413446815a2b4cdd5d0d39e8ce0eef2b7885
StanislavSolovei <edukacjateb176@gmail.com>
2026-09-01 18:57:18 +0200

    Not all ❌

---
+++ obiekty.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Islandia</title>
+</head>
+<body>
+    <header>
+        <h1><a href="islandia.php">Zwiedzaj Islandię</a></h1>
+    </header>
+    <aside>
+        <h3>Do zwiedzania</h3>
+        <ul>
+            <li>Wodospady:
+                <ol><!-- Skrypt 1 --></ol>
+            </li>
+            <li>Siedliska zwierząt:
+                <ol><!-- Skrypt 2 --></ol>
+            </li>
+        </ul>
+    </aside>
+    
+    <main>
+        <h2>Galeria</h2>
+        <section><!-- Skrypt 4 --></section>
+    </main>
+    <footer>
+        <hr>
+        <p>Autor: 00000000000</p>
+    </footer>
+</body>
+</html>
